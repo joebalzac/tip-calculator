d2bdf103aaafc1d1c3f17ff20f1e60073f8292de
joebalzac <jgbalzac@gmail.com>
2021-08-26 16:45:03 -0400

    bring it back homie

---
+++ app/js/script.js
@@ -0,0 +1,43 @@
+//Variables
+const billAmount = document.querySelector("#bill");
+const peopleAmount = document.querySelector("#people");
+const billTipAmount = document.querySelector(".tip-amount-value");
+const billTotalAmount = document.querySelector(".total-value");
+const buttons = document.querySelectorAll(".tip-section button");
+const custom = document.querySelector("#custom");
+const resetButton = document.querySelector(".reset");
+
+//Calculate Tip When Click On Tip Percentage Buttons
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let tipvalue = e.target.innerText;
+    tipvalue = tipvalue.substr(0, tipvalue.length - 1);
+
+    if (billAmount.value === "") return;
+    if (peopleAmount.value === "") peopleAmount.value = 1;
+
+    calculateTip(
+      parseFloat(billAmount.value),
+      parseInt(tipvalue),
+      parseInt(peopleAmount.value)
+    );
+  });
+});
+
+//Calculate Tip When User Give Custom Tip Percentage Input
+
+//Calculate Tip
+
+function calculateTip(billAmount, tipPercentage, peopleAmount) {
+  let tipAmount = (billAmount * (tipPercentage / 100)) / peopleAmount;
+  let tip = Math.floor(tipAmount * 100) / 100;
+  tip = tip.toFixed(2);
+
+  let totalAmount = (billAmount * (tipAmount * 100)) / 100;
+  totalAmount = totalAmount.toFixed(2);
+
+  billTipAmount.innerHTML = `$${tip}`;
+  billTotalAmount.innerHTML = `$${totalAmount}`;
+}
+//Reset Everything
